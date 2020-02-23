b25e43580e83de4cd81c5fbbe668baef4fbdedb3
Chengwen <myregs@foxmail.com>
2020-02-23 12:20:48 -0800

    Removed react-alert component(too heavy) and fixed re-fetch history data by adding an error state

--- src/components/LineChart.js
+++ src/components/LineChart.js
@@ -4,18 +4,30 @@ import echarts from 'echarts/lib/echarts';
 import 'echarts/lib/chart/line';
 import axios from 'axios';
 import moment from 'moment';
-import { useAlert } from 'react-alert'
+
+// import Alert from '@material-ui/lab/Alert';
+// import { makeStyles } from '@material-ui/core/styles';
+
+// const useStyles = makeStyles(theme => ({
+//   root: {
+//     width: '100%',
+//     '& > * + *': { marginTop: theme.spacing(2),},
+//     textAlign: 'center',
+//   },
+// }));
 
 export default function ChinaTrend() {
   
   const [data, setData] = useState([]);
-  const [loaded, setReady] = useState(false); 
+  const [loaded, setReady] = useState(false);  // flag for loading map
+  const [error, setError] = useState(false);  // dependency flag for re-send request
   
-  const alert = useAlert();
+  // const classes = useStyles();
+
   useEffect(() => {
     const hisDataUrl = 'https://www.windquant.com/qntcloud/data/edb?userid=2a5db344-6b19-4828-9673-d0d81bd265bc';
     const indicators = '&indicators=S6274770,S6274773,S6274772,S6274771&startdate=2020-01-20&enddate=';
-    const endDate = moment(new Date()).format('YYYY-MM-DD');
+    const endDate = moment().format('YYYY-MM-DD');
 
     axios.get(hisDataUrl + indicators + endDate).then( hisData => {
       if(hisData.data.errCode === 0) {
@@ -26,15 +38,15 @@ export default function ChinaTrend() {
           curesNum: hisData.data.data[2],
           deathsNum: hisData.data.data[3]
         });
+        setReady(true);
       } else {
-        console.log('Requst for history data in China:', hisData.data.errCode);
-        alert.show('Opps... Failed to get history data!');
+        console.log('Requst for history data in China, errCode:', hisData.data.errCode);
+        setError(true);  // this will activate another request
       }
-      setReady(true);
     }).catch(e => { console.log('Request history data in China', e) });
 
-    return (console.log('Cleanup'));
-  },[alert]);
+    // return (console.log('Cleanup'));
+  },[error]);
 
   const getLoadingOption = () => {
     return { text: 'Data Loading ...' };
@@ -100,16 +112,22 @@ export default function ChinaTrend() {
   }
 
   return (
-    <ReactEcharts 
-      style={{height: "83vh"}}
-      echarts={echarts}
-      option={getOption()}
-      loadingOption={getLoadingOption()}
-      onChartReady={onChartReady}
-      showLoading={!loaded}  //official showloading always have bugs 
-      notMerge={true}
-      lazyUpdate={true}
-      theme={"theme_name"}
-    />
+    // (!loaded && error) ?
+    // (<div className={classes.root}>
+    //   <Alert severity="warning">Sorry, failed to get history data, please Refresh page!</Alert>
+    //  </div>) :
+    (
+      <ReactEcharts 
+        style={{height: "83vh"}}
+        echarts={echarts}
+        option={getOption()}
+        loadingOption={getLoadingOption()}
+        onChartReady={onChartReady}
+        showLoading={!loaded}  //official showloading always have bugs 
+        notMerge={true}
+        lazyUpdate={true}
+        theme={"theme_name"}
+      />
+    )
   )
 }
