49e25b0de54d72cac6a705d9c927bb61432db4b4
Chengwen <myregs@foxmail.com>
2020-03-06 17:22:31 -0800

    Added dynamic barchart and timeline for map view

--- src/components/MapCanada.js
+++ src/components/MapCanada.js
@@ -6,9 +6,13 @@ import 'echarts/lib/chart/line';
 import axios from 'axios'
 import cheerio from 'cheerio';
 
+//  data: ['Ontario', 'British Columbia', 'Quebec', 'Alberta', 'Saskachewan', 'Manitoba', '冰岛', '日本', '美国', '(131)中国']
+
 export default function MapCanada() {
-  const [data, setData] = useState([]);
+  // const [data, setData] = useState([]);
   const [loaded, setReady] = useState(false);
+  const [cases, setCases] = useState({dates:[], data:[]});
+
 
   useEffect(() => {
     import(`../assets/CanadaGEO.json`).then(map => {
@@ -17,24 +21,37 @@ export default function MapCanada() {
   }, []);
 
   useEffect(() => {
-      axios.get("https://www.canada.ca/en/public-health/services/diseases/2019-novel-coronavirus-infection.html")
-      .then( response => {
-          if(response.status === 200) {
-            let curCases = [];
-            const $ = cheerio.load(response.data);
-            $(".table").each( (index, ele) => {
-              if(index === 0)              
-                $('tr', 'tbody', ele).each( (index, ele) => {
-                  let province = $(ele).text().trim().split("\n")[0];
-                  let cases = $(ele).text().trim().split("\n")[1].replace(/ /g, '');
-                  curCases.push({name: province, value: cases});
-                });
-            });
-            setData(curCases);
-            setReady(true);
-          }
-      });
+    import(`../assets/CasesDb.json`).then( ({cases}) => {
+      setCases(
+        {
+          dates: cases.map(day => day.date),     // for timeline component
+          data: cases.map(day => day.cases),  // for map component
+          // barData: cases.map(day => day.cases.map(prov => prov.value))  // for bar Chart component
+        }
+      );
+      setReady(true);
+    });
   }, []);
+
+  // useEffect(() => {
+  //     axios.get("https://www.canada.ca/en/public-health/services/diseases/2019-novel-coronavirus-infection.html")
+  //     .then( response => {
+  //         if(response.status === 200) {
+  //           let curCases = [];
+  //           const $ = cheerio.load(response.data);
+  //           $(".table").each( (index, ele) => {
+  //             if(index === 0)              
+  //               $('tr', 'tbody', ele).each( (index, ele) => {
+  //                 let province = $(ele).text().trim().split("\n")[0];
+  //                 let cases = $(ele).text().trim().split("\n")[1].replace(/ /g, '');
+  //                 curCases.push({name: province, value: cases});
+  //               });
+  //           });
+  //           setData(curCases);
+  //           setReady(true);
+  //         }
+  //     });
+  // }, []);
 
   const getLoadingOption = () => {
     return {
@@ -43,93 +60,148 @@ export default function MapCanada() {
   };
 
   const onChartReady = (chart) => {
-    if(Array.isArray(data) && data.length > 0) {
+    if(Array.isArray(cases) && cases.length > 0) {
       setTimeout(() => { chart.hideLoading(); }, 1500);
     }
   };
 
-  const getOption = () => {
+  const setVisualMap = () => {
     return {
-      // backgroundColor: '#C0C0C0',
-      title:  {
+      show: true,
+      type: 'piecewise',
+      min: 0,
+      max: 100000,
+      align: 'right',
+      bottom: '7%',
+      left: 'center',
+      orient: 'horizontal',
+      inRange: { color: [ '#ffc0b1', '#ff8c71', '#ef1717', '#9c0505' ] },
+      pieces: [ 
+        {min: 100},
+        {min: 50, max: 99},
+        {min: 20, max: 49},
+        {min: 10, max: 19},
+        {min: 6, max: 9},
+        {min: 0, max: 5},
+      ],
+      padding: 30,
+      showLabel: true,
+      text: ['Outbreak', 'Minor Break'],
+      itemWidth: 10,
+      itemHeight: 10,
+      textStyle: { fontSize: 12, fontWeight: 'bold' }
+    }
+  }
+
+  const getOptionConfig = () => {
+    return {
+      //global settings
+      baseOption: {
+        timeline: {  // by day
+          //loop: false,      
+          axisType: 'category',
+          show: true,
+          autoPlay: true,
+          playInterval: 1500,
+          data: cases.dates,        // Days since the 1st cases
+          currentIndex: cases.dates.length - 1,
+        },  
+        title:  {
           x: 'center',
           text: 'Cases by Province in Canada',
           subtext: 'Data from https://www.canada.ca/en/public-health',
           textStyle: {fontSize: 18},
-      },
-      visualMap: {
-        show: true,
-        type: 'piecewise',
-        min: 0,
-        max: 100000,
-        align: 'left',
-        top: '5%',
-        left: 'center',
-        inRange: { color: [ '#ffc0b1', '#ff8c71', '#ef1717', '#9c0505' ] },
-        // cases number ranges: greater number indicates more severe epidemic area
-        pieces: [ 
-          {min: 200},
-          {min: 100, max: 199},
-          {min: 50, max: 99},
-          {min: 20, max: 49},
-          {min: 6, max: 19},
-          {min: 1, max: 5},
-        ],
-        padding: 30,
-        orient: 'horizontal',
-        showLabel: true,
-        text: ['Outbreak', 'Minor Break'],
-        itemWidth: 10,
-        itemHeight: 10,
-        textStyle: { fontSize: 12, fontWeight: 'bold' }
-      },
-      toolbox: { feature: { saveAsImage: {} } },
-      tooltip: {
-        formatter: (params) => {
-          let value = ((params.value || "No Case") + '').split('.');
-          value = value[0].replace(/(\d{1,3})(?=(?:\d{3})+(?!\d))/g, '$1,');
-          return `<b>${params.name}</b><br />Confirmed: ${value}<br />`;
-        }
-      },
-      // geo: {  },
-      series: [{
-        left: 'center',
-        top: '19%',
-        type: 'map',
-        name: '',
-        geoIndex: 0,
-        data: data, // area(provinces) data
-        map: 'Canada',
-        silent: false, // province area is clickable
-        label: { normal: { show: true, fontSize:'8', color: 'rgba(0,0,0,0.7)' }}, 
-        itemStyle: {
-          normal:{ 
-            borderColor: 'rgba(0, 0, 0, 0.2)',
-            areaColor: '#B2E5BC'  // default area color 
-          },
-          emphasis:{
-              areaColor: '#53adf3', // change color when click
-              shadowOffsetX: 0,
-              shadowOffsetY: 0,
-              shadowBlur: 20,           
-              borderWidth: 0,
-              shadowColor: 'rgba(0, 0, 0, 0.5)'
-          }
         },
-        mapType: 'map',        
-        zoom: 1.1,
-        roam: false,
-        showLegendSymbol: false,
-        rippleEffect: { show: true, brushType: 'stroke', scale: 2.5, period: 4 },
-      }]
+        tooltip: {
+          show: true,
+          trigger: 'item',
+        },
+        visualMap: setVisualMap(),   
+        xAxis: [{
+          type: 'value',
+          max: 100,
+          axisLine: { show: false },
+          axisTick: { show: false },
+          axisLabel: { show: false },
+          splitLine: { show: false },
+        },],
+        yAxis: [{
+          type: 'category',
+          inverse: true,
+          data: ['ON', 'BC', 'QC', 'AB', 'MB', 'SK', 'NL', 'PE', 'NS', 'NB', 'YT', 'NT', 'NU']
+        }],
+        series: [
+          {
+            type: 'bar',  
+            label: {
+              position: 'inside',
+              show: true,
+              color: 'blue'
+            },
+          },
+          {
+            left: '20%',
+            top: '19%',
+            type: 'map',
+            name: '',
+            geoIndex: 0,
+            // data: data, // area(provinces) data, put this into OPTIONS' series
+            map: 'Canada',
+            silent: false, // province area is clickable
+            label: { normal: { show: true, fontSize:'8', color: 'rgba(0,0,0,0.7)' }}, 
+            itemStyle: {
+              normal:{ 
+                borderColor: 'rgba(0, 0, 0, 0.2)',
+                areaColor: '#B2E5BC'  // default area color 
+              },
+              emphasis:{
+                  areaColor: '#53adf3', // change color when click
+                  shadowOffsetX: 0,
+                  shadowOffsetY: 0,
+                  shadowBlur: 20,           
+                  borderWidth: 0,
+                  shadowColor: 'rgba(0, 0, 0, 0.5)'
+              }
+            },
+            mapType: 'map',        
+            zoom: 1.1,
+            roam: false,
+            showLegendSymbol: false,
+            rippleEffect: { show: true, brushType: 'stroke', scale: 2.5, period: 4 },
+          }
+        ]
+      },          
+    
+      // played data changes here, one timespot(day) one data
+      options: cases.data.map( (dayCases, index) => {
+        return {
+          title: { text: 'Cases by Province in Canada on ' + cases.dates[index] + ', 2020', }, //by day          
+          series: 
+          [
+            {
+              data: dayCases.map(prov => prov.value) // for barchart(province data)
+            },
+            {
+              data: dayCases, // for map
+              tooltip: {
+                formatter: (params) => {
+                  let value = ((params.value || "No Case") + '').split('.');
+                  value = value[0].replace(/(\d{1,3})(?=(?:\d{3})+(?!\d))/g, '$1,');
+                  return `<b>${params.name}</b><br />Confirmed: ${value}<br />`;
+                }
+              },
+            },
+          ]
+        }
+      }),            
     }
   }
-
+  
   return (
     <ReactEcharts 
       style={{height: "85vh"}}
       echarts={echarts}
-      option={getOption()}
+      option={getOptionConfig()}
       loadingOption={getLoadingOption()}
       onChartReady={onChartReady}
       showLoading={!loaded}
@@ -141,4 +213,7 @@ export default function MapCanada() {
     />
   )
 }
+
+
+
 
