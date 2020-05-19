f4ff9bc145edc69a0a73306d8c25a5d8beecb468
Chengwen <myregs@foxmail.com>
2020-05-18 20:44:04 -0700

    Commented the Zoom to fit feature of mobile view

--- src/index.js
+++ src/index.js
@@ -5,15 +5,16 @@ import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
-const getPageZoomRatio = () => {
-	// if (document.body.clientWidth > 1024) return "70%";
-	return (
-		Math.floor((document.body.clientWidth * 100) / window.innerWidth) + "%"
-	);
-};
+// const getPageZoomRatio = () => {
+// 	// if (document.body.clientWidth > 1024) return "70%";
+// 	return (
+// 		Math.floor((document.body.clientWidth * 100) / window.innerWidth) + "%"
+// 	);
+// };
 
 // console.log(getPageZoomRatio());
 
 // This property has the same effect with CSS zoom property
 // It's not the same effect as Page Zoom in Browser
-document.body.style.zoom = getPageZoomRatio();
+// Issues: This will effect the echarts map tap/click focus
+// document.body.style.zoom = getPageZoomRatio();
