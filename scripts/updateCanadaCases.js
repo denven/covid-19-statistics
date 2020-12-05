5c1e40b06659622d3871c8f4e99294cd58a83321
Chengwen <myregs@foxmail.com>
2020-12-05 01:42:25 -0800

    Added year for Canada script and data

--- scripts/updateCanadaCases.js
+++ scripts/updateCanadaCases.js
@@ -448,7 +448,7 @@ async function updateHistoryCasesV2() {
 	// console.log(casesAsOfToday);
 
 	let date = new Date();
-	let dateString = date.getMonth() + 1 + "/" + date.getDate();
+	let dateString = date.getFullYear() + date.getMonth() + 1 + "/" + date.getDate();
 
 	if (casesAsOfToday.length > 0) {
 		if (dateString !== allDaysCases[allDaysCases.length - 1].date)
